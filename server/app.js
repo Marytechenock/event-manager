a28c627cf51e618805288823907b5edf70342c14
unknown <richardmutero144@gmail.com>
2025-11-13 15:22:53 +0200

    Add uniform sidebar

--- server/app.js
+++ server/app.js
@@ -64,6 +64,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+
+app.get('/admin/', (req, res) => {
+  res.redirect('/admin-login.html');
+});
+
 app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/admin-login.html'));
 });
